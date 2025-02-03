55d4ab6a3ceaf78503ea92d49bae5cdaccf400f8
Игорь Гордеев <igor@MacBook-Pro-Igor.local>
2025-02-03 19:29:49 +0300

    подключение к DB и прописан функционал путей App.jsx

--- server/index.js
+++ server/index.js
@@ -2,10 +2,14 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const authRoutes = require("./routes/auth");
+const connectDB = require("./db"); // Подключение к базе данных
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
+
+// Подключение к базе данных
+connectDB();
 
 // Маршруты
 app.use("/api/auth", authRoutes);
